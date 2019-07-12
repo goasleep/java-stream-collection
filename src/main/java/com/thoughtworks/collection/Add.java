9f1a3a2c47fa8e14b4814f2b50a9bec896880f14
SMITH PENG (ITA-ISDC-ISD-OOCLL/ZHA) <PENGSM@OOCL.COM>
2019-07-12 17:44:58 +0800

    implement the 3 practice

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,22 +3,28 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int min = leftBorder < rightBorder ? leftBorder : rightBorder;
+        int number = Math.abs(leftBorder - rightBorder) + 1;
+        return Stream.iterate(min, n -> n + 1).limit(number).filter(n -> n % 2 == 0).reduce(0, Integer::sum);
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int min = leftBorder < rightBorder ? leftBorder : rightBorder;
+        int number = Math.abs(leftBorder - rightBorder) + 1;
+        return Stream.iterate(min, n -> n + 1).limit(number).filter(n -> n % 2 == 1).reduce(0, Integer::sum);
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(n -> n % 2 == 1).map(n -> n * 3 + 2).reduce(0, Integer::sum);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(n -> n % 2 == 1 ? 3 * n + 2 : n).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
