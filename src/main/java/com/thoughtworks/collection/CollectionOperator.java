267d057175a3adfd0aad5b0844cd309417758d99
SMITH PENG (ITA-ISDC-ISD-OOCLL/ZHA) <PENGSM@OOCL.COM>
2019-07-14 10:23:03 +0800

    popLastElment

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,23 +15,23 @@ public class CollectionOperator {
         int size = Math.abs(left - right) + 1;
         if (left < right) {
             return Stream.iterate(left, n -> n + 1).limit(size).collect(Collectors.toList());
-        }else {
+        } else {
             return Stream.iterate(left, n -> n - 1).limit(size).collect(Collectors.toList());
         }
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
         CollectionOperator collectionOperator = new CollectionOperator();
-        return collectionOperator.getListByInterval(left,right).stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        return collectionOperator.getListByInterval(left, right).stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        return Arrays.stream(array).filter(n -> n % 2 == 0).collect(ArrayList::new,ArrayList::add,ArrayList::addAll);
+        return Arrays.stream(array).filter(n -> n % 2 == 0).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
     }
 
     public int popLastElment(int[] array) {
-
+        return Arrays.stream(array).reduce(0,(first, second) -> second);
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
